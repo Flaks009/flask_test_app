adb60d9254197c30ba91f8a2e9ff4676c65eef8c
flaks009 <flaks009@gmail.com>
2020-06-15 16:52:05 -0300

    Marca BD and View

--- app/db.py
+++ app/db.py
@@ -7,3 +7,9 @@ def list_rpi():
     items = pd.read_sql_query('SELECT * FROM rpi', con)
     items = items.to_dict()
     return items
+
+def list_marca():
+    con = psycopg2.connect(host = DB.HOST, port=DB.PORT, user=DB.USER, password=DB.PASSWORD, database=DB.DATABASE)
+    items = pd.read_sql_query('SELECT * FROM marca', con)
+    items = items.to_dict()
+    return items
--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ import os
 from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm, TestForm, UploadForm
-from app.db import list_rpi
+from app.db import list_rpi, list_marca
 from werkzeug.utils import secure_filename
 
 @app.route('/')
@@ -12,8 +12,13 @@ def index():
     rpi = list_rpi()
     return render_template('index.html', title = 'Home', rpi = rpi)
 
-def allowed_file(filename):
+@app.route('/marca')
+def marca():
+    marca = list_marca()
+    return render_template('marca.html', title = 'Marca', marca = marca)
 
+
+def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
