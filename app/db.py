11612d55844db4ef356674982781d65d951de80e
flaks009 <flaks009@gmail.com>
2020-06-15 12:35:30 -0300

    RPI Database

--- app/db.py
+++ app/db.py
@@ -2,14 +2,6 @@ from app.db_config import DB
 import psycopg2
 import pandas as pd
 
-def insert(string_t):
-    con = psycopg2.connect(dbname=DB.DBNAME, user=DB.USER, host=DB.HOST, password=DB.PASSWORD)
-    cur = con.cursor()
-    cur.execute("INSERT INTO test (string_t) VALUES(%s)", (string_t, ))
-    con.commit()
-    cur.close()
-    con.close()
-
 def list_rpi():
     con = psycopg2.connect(host = DB.HOST, port=DB.PORT, user=DB.USER, password=DB.PASSWORD, database=DB.DATABASE)
     items = pd.read_sql_query('SELECT * FROM rpi', con)
