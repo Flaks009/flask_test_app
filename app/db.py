e9a8a2725922f490f149e88f9bdc4c662ed09536
flaks009 <flaks009@gmail.com>
2020-06-16 00:51:34 -0300

    Patente, Desenho e Marca rpi_query

--- app/db.py
+++ app/db.py
@@ -25,3 +25,21 @@ def list_desenho():
     items = pd.read_sql_query('SELECT * FROM desenho', con)
     items = items.to_dict()
     return items
+
+def rpi_desenho(rpi_cod):
+    con = psycopg2.connect(host = DB.HOST, port=DB.PORT, user=DB.USER, password=DB.PASSWORD, database=DB.DATABASE)
+    items = pd.read_sql_query('SELECT * FROM desenho where numero_rpi = %s' % (rpi_cod), con)
+    items = items.to_dict()
+    return items
+
+def rpi_marca(rpi_cod):
+    con = psycopg2.connect(host = DB.HOST, port=DB.PORT, user=DB.USER, password=DB.PASSWORD, database=DB.DATABASE)
+    items = pd.read_sql_query('SELECT * FROM marca where numero_rpi = %s' % (rpi_cod), con)
+    items = items.to_dict()
+    return items
+
+def rpi_patente(rpi_cod):
+    con = psycopg2.connect(host = DB.HOST, port=DB.PORT, user=DB.USER, password=DB.PASSWORD, database=DB.DATABASE)
+    items = pd.read_sql_query('SELECT * FROM patente where numero_rpi = %s' % (rpi_cod), con)
+    items = items.to_dict()
+    return items
--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ import os
 from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm, TestForm, UploadForm
-from app.db import list_rpi, list_marca, list_desenho, list_patente
+from app.db import list_rpi, list_marca, list_desenho, list_patente, rpi_desenho, rpi_marca, rpi_patente
 from werkzeug.utils import secure_filename
 
 @app.route('/')
@@ -25,6 +25,21 @@ def desenho():
 @app.route('/patente')
 def patente():
     patente = list_patente()
+    return render_template('patente.html', title = 'Patente', patente = patente)
+
+@app.route('/get_desenho_rpi')
+def get_desenho_rpi():
+    desenho = rpi_desenho(request.form['desenho_rpi'])
+    return render_template('desenho.html', title = 'Desenho', desenho = desenho)
+
+@app.route('/get_marca_rpi')
+def get_marca_rpi():
+    marca = rpi_marca(request.form['marca_rpi'])
+    return render_template('marca.html', title = 'Marca', marca = marca)
+
+@app.route('/get_patente_rpi')
+def get_patente_rpi():
+    patente = rpi_patente(request.form['patente_rpi'])
     return render_template('patente.html', title = 'Patente', patente = patente)
 
 def allowed_file(filename):
