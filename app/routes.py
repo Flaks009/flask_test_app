7ca758967843d1385bd7df16aeda84bf2af62706
flaks009 <flaks009@gmail.com>
2020-07-02 12:21:59 -0300

    Ajax request

--- app/routes.py
+++ app/routes.py
@@ -48,8 +48,7 @@ def get_patente_rpi():
 
 @app.route('/post_insert_email', methods=['POST'])
 def post_insert_email():
-    email = request.json['email']
-    num_ped = request.json['num_ped']
-    rpi = request.json['rpi']
+    email = request.args.get['email']
+    num_ped = request.args.get['num_ped']
     insert_email_desenho(num_ped, email)
     return '', 204
