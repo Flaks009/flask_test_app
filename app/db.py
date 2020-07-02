d8f1aff53d9ed6edf9f8f0baff25122a68aecfd4
flaks009 <flaks009@gmail.com>
2020-07-02 18:49:32 -0300

    Only get procurador null

--- app/db.py
+++ app/db.py
@@ -28,19 +28,19 @@ def list_desenho():
 
 def rpi_desenho(rpi_cod):
     con = psycopg2.connect(host = DB.HOST, port=DB.PORT, user=DB.USER, password=DB.PASSWORD, database=DB.DATABASE)
-    items = pd.read_sql_query("SELECT * FROM desenho where numero_rpi = '%s' AND nome_do_procurador is NULL" % (rpi_cod), con)
+    items = pd.read_sql_query("SELECT * FROM desenho where numero_rpi = '%s' AND nome_do_procurador IS NULL OR nome_do_procurador=''" % (rpi_cod), con)
     items = items.to_dict()
     return items
 
 def rpi_marca(rpi_cod):
     con = psycopg2.connect(host = DB.HOST, port=DB.PORT, user=DB.USER, password=DB.PASSWORD, database=DB.DATABASE)
-    items = pd.read_sql_query("SELECT * FROM marca where numero_rpi = '%s' AND nome_do_procurador is NULL" % (rpi_cod), con)
+    items = pd.read_sql_query("SELECT * FROM marca where numero_rpi = '%s' AND nome_do_procurador IS NULL OR nome_do_procurador=''" % (rpi_cod), con)
     items = items.to_dict()
     return items
 
 def rpi_patente(rpi_cod):
     con = psycopg2.connect(host = DB.HOST, port=DB.PORT, user=DB.USER, password=DB.PASSWORD, database=DB.DATABASE)
-    items = pd.read_sql_query("SELECT * FROM patente where numero_rpi = '%s' AND nome_do_procurador is NULL" % (rpi_cod), con)
+    items = pd.read_sql_query("SELECT * FROM patente where numero_rpi = '%s' AND nome_do_procurador IS NULL OR nome_do_procurador=''" % (rpi_cod), con)
     items = items.to_dict()
     return items
 
