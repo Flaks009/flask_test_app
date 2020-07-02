2a621343155798ed966208a81f1f0040705ff70e
flaks009 <flaks009@gmail.com>
2020-07-02 18:28:36 -0300

    Ajax request

--- app/routes.py
+++ app/routes.py
@@ -32,9 +32,6 @@ def get_desenho_rpi():
     if request.method == 'POST':
         desenho = rpi_desenho(request.form['desenho_rpi'])
         return render_template('desenho.html', title = 'Desenho', desenho = desenho)
-    elif request.method == 'GET':
-        desenho = rpi_desenho(request.args.get('rpi'))
-        return render_template('desenho.html', title = 'Desenho', desenho = desenho)
 
 @app.route('/get_marca_rpi', methods=['POST'])
 def get_marca_rpi():
@@ -52,4 +49,5 @@ def post_insert_email():
     num_ped = request.json['num_ped']
     rpi = request.json['rpi']
     insert_email_desenho(num_ped, email)
-    return redirect(url_for('get_desenho_rpi'))
+    desenho = rpi_desenho(rpi)
+    return {'json': desenho}
