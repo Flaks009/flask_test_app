5c309e7beeb2f03a8e364ccb0aeb6b6f3f490fc1
flaks009 <flaks009@gmail.com>
2020-07-02 18:23:19 -0300

    Ajax request

--- app/routes.py
+++ app/routes.py
@@ -31,10 +31,7 @@ def patente():
 def get_desenho_rpi():
     if request.method == 'POST':
         desenho = rpi_desenho(request.form['desenho_rpi'])
-        return render_template('desenho.html', title = 'Desenho', desenho = desenho, teste="dircet")
-    elif request.method == 'GET':
-        desenho = rpi_desenho(request.args.get('rpi'))
-        return render_template('desenho.html', title = 'Desenho', desenho = desenho, teste="indirect")
+        return render_template('desenho.html', title = 'Desenho', desenho = desenho, teste="direct")
 
 @app.route('/get_marca_rpi', methods=['POST'])
 def get_marca_rpi():
@@ -52,4 +49,5 @@ def post_insert_email():
     num_ped = request.json['num_ped']
     rpi = request.json['rpi']
     insert_email_desenho(num_ped, email)
-    return redirect(url_for('get_desenho_rpi', rpi=rpi))
+    desenho = rpi_desenho(rpi)
+    return render_template('desenho.html', title = 'Desenho', desenho = desenho, teste="indirect")
