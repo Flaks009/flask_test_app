c04ee14b11a8634f982c5c652958a2290544fbdd
flaks009 <flaks009@gmail.com>
2020-07-02 16:46:50 -0300

    Ajax request

--- app/static/js/datatables.js
+++ app/static/js/datatables.js
@@ -1,4 +1,5 @@
-var data = JSON.parse('{{json}}')
+console.log('aaaaa');
+var data = JSON.parse('{{desenho | tojson | safe}}')
 var table = $('#data-table').DataTable({});
 var span = $('#rpi').text(data.numero_rpi[0])
 
@@ -16,12 +17,12 @@ function dataAdd(data) {
             data.nome_do_procurador[i],
             data.email[i],
             `
-                <form id="${id_html}">
-                <input name="email"><input type="hidden" value="${$('#rpi')[0].innerHTML}" name="rpi">
-                <input type="hidden" value="${data.numero_do_pedido[i]}" name="num_ped">
-                <button type="button" class="btn btn-primary" onclick="post_email('${id_html}')">Inserir</button>
-                </form>
-                `
+            <form id="${id_html}">
+            <input name="email"><input type="hidden" value="${$('#rpi')[0].innerHTML}" name="rpi">
+            <input type="hidden" value="${data.numero_do_pedido[i]}" name="num_ped">
+            <button type="button" class="btn btn-primary" onclick="post_email('${id_html}')">Inserir</button>
+            </form>
+            `
         ]);
     });
 
