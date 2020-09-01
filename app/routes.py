2434131a69fdee24ad680c3fbe951f8ca5a9585b
flaks009 <flaks009@gmail.com>
2020-09-01 18:04:40 -0300

    Files

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,10 @@
 import os
 from flask import render_template, flash, redirect, url_for, request, jsonify
 from app import app
+import glob
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title = 'Home')
+    files = glob.glob('/home/ubuntu/inpi_extract/xlsx/*')
+    return render_template('index.html', title = 'Home', files = files)
