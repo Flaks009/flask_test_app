a7138d5a1e737e2aae1b311a91b13f94c6b9ce99
flaks009 <flaks009@gmail.com>
2020-09-01 18:25:35 -0300

    Download FIles

--- app/routes.py
+++ app/routes.py
@@ -4,12 +4,14 @@ from flask import render_template, flash, redirect, url_for, request, jsonify, s
 from app import app
 import glob
 
+UPLOAD_DIRECTORY = "/home/ubuntu/inpi_extract/xlsx/"
+
 @app.route('/')
 @app.route('/index')
 def index():
     files = glob.glob('/home/ubuntu/inpi_extract/xlsx/*')
     return render_template('index.html', title = 'Home', files = files)
 
-@app.route('/<path>')
-def download(path = None):
-    return send_file(path, as_attachment=True)
+@app.route('/download/<path:path>')
+def download(path):
+    return send_file(UPLOAD_DIRECTORY, path, as_attachment=True)
